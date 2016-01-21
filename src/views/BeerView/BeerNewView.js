b54bce68e327e944ecff8619e53d3183bfc8a8c5
tyleriguchi <tyleriguchi@gmail.com>
2016-01-21 12:50:16 -0800

    create new beer action

--- src/views/BeerView/BeerNewView.js
+++ src/views/BeerView/BeerNewView.js
@@ -2,26 +2,37 @@ import React, { Component, PropTypes} from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { routeActions } from 'redux-simple-router';
 import * as BeerActions from '../../redux/actions/Beers';
+import BeerNewForm from '../../components/BeerNewForm';
 
 class BeerCreate extends Component {
   constructor(props) {
     super(props);
   }
 
-  render() {
-    const { actions } = this.props;
-    let beer = { name: 'mine'};
+  createBeer(beer) {
+    const { beerActions, routeActions } = this.props;
 
+    beerActions.addBeer(beer);
+    routeActions.push('/');
+  }
+
+  render() {
     return (
-      <button onClick={ () => actions.addBeer(beer)}>CreateBeer</button>
+      <div>
+        <BeerNewForm
+          createBeer={ this.createBeer.bind(this) }
+        />
+      </div>
     )
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(BeerActions, dispatch)
+    beerActions: bindActionCreators(BeerActions, dispatch),
+    routeActions: bindActionCreators(routeActions, dispatch)
   }
 }
 
